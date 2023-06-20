aca2cc91d2697ec507cdf3282d855ea3347d1cbe
Guglielmo Caliendo <guglielmocaliendo92@gmail.com>
2023-06-20 13:01:49 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World in Jenkins");
-        System.out.println(5+5)
+        System.out.println(5+5);
     }
 }
